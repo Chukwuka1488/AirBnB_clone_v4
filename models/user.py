a99c3ff66a1d85d4abd84b1ac22b5c9909c06c81
Chukwuka Akibor <juniorakibor@yahoo.com>
2024-01-10 10:02:14 -0800

    security improvements

--- models/user.py
+++ models/user.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ holds class User"""
+from hashlib import md5
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
@@ -24,6 +25,20 @@ class User(BaseModel, Base):
         first_name = ""
         last_name = ""
 
+    @property
+    def password(self):
+        """Getter for the password attribute"""
+        return self.__password
+
+    @password.setter
+    def password(self, pwd):
+        """Setter for the password attribute, hashes the password to a MD5 value"""
+        self.__password = md5(pwd.encode()).hexdigest()
+
+
     def __init__(self, *args, **kwargs):
         """initializes user"""
+        pwd = kwargs.pop("password", None)
         super().__init__(*args, **kwargs)
+        if pwd:
+            self.password = pwd
