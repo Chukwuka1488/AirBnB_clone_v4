1b068481018e15cc32967ac7b04b73e55b1798ff
Chukwuka Akibor <juniorakibor@yahoo.com>
2024-01-10 09:49:20 -0800

    view for the link between Place objects and Amenity objects that handles all default RESTFul API actions

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,6 +5,7 @@ Flask API Setup
 
 from flask import Flask, jsonify, make_response, Blueprint
 from models import storage
+from flask_cors import CORS
 from models.engine import *
 from api.v1.views import app_views
 from os import getenv
@@ -12,7 +13,8 @@ from os import getenv
 
 """ Flask instances """
 app = Flask(__name__)
-
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
 
 
@@ -25,10 +27,14 @@ def teardown_close(error):
 @app.errorhandler(404)
 def not_found(error):
     """ function to handle error 404 """
-    return make_response(jsonify({'error': "Not found"}), 404)
+    return jsonify({'error': "Not found"}), 404
 
 
 if __name__ == '__main__':
-    host = getenv('HBNB_API_HOST', '0,0,0,0')
-    port = getenv('HBNB_API_PORT', '5000')
-    app.run(host=host, port=port, threaded=True, debug=True)
+    host = getenv("HBNB_API_HOST")
+    if host is None:
+        host = '0.0.0.0'
+    port = getenv("HBNB_API_PORT")
+    if port is None:
+        port = '5000'
+    app.run(host=host, port=port, threaded=True)
