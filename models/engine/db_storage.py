55cb089e101ca0d778cdd221d0b148df10205492
Chukwuka Akibor <juniorakibor@yahoo.com>
2024-01-10 08:31:42 -0800

    improve storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -4,6 +4,7 @@ Contains the class DBStorage
 """
 
 import models
+import json
 from models.amenity import Amenity
 from models.base_model import BaseModel, Base
 from models.city import City
@@ -74,3 +75,20 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """
+        Returns the object based on the class and its ID, or None if not found
+        """
+        return_all = self.all(cls)
+        for value in return_all.values():
+            if value.id == id:
+                return value
+        return None
+
+    def count(self, cls=None):
+        """
+        Returns the number of objects in storage matching the given class.
+        If no class is passed, returns the count of all objects in storage.
+        """
+        return (len(self.all(cls)))
